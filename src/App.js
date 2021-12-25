f8bbbc0fc5ec4b7c2cefdcd1f44aa178033dd087
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-26 01:36:25 +0430

    Design login page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { Route, Routes } from 'react-router';
 import Login from './components/Login';
 import './App.css';
 import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header" />
       <Routes>
         <Route path="/" exact element={<Home />} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => <h1>Login</h1>;
+const Login = () => {
+  function login(e) {
+    e.preventDefault();
+    console.log('login called');
+  }
+  return (
+    <div className="row justify-content-center mt-5">
+      <form className="col-6">
+        <h1 className="my-3">Login</h1>
+        <input className="form-control my-2" type="text" name="username" placeholder="username" />
+        <input className="form-control my-2" type="text" name="password" placeholder="password" />
+        <button
+          className="btn-primary rounded px-3 m-2 align-self-center"
+          type="submit"
+          onClick={(e) => {
+            login(e);
+          }}
+        >
+          submit
+        </button>
+      </form>
+    </div>
+  );
+};
 
 export default Login;
