58ba091dce4d091ffa2c56e9333d2ad9026c171b
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 21:10:38 +0430

    Correct spelling for some words

--- src/redux/reposReducer.js
+++ src/redux/reposReducer.js
@@ -9,30 +9,32 @@ export const SORT = 'spacebox/redux/reposReducer/SORT_REPOS';
 export const SEARCH = 'spacebox/redux/reposReducer/SEARCH_REPOS';
 const REMOVE = 'spacebox/redux/reposReducer/REMOVE_REPO';
 
-export const fetchWatchedRepos = () => (disptach) => {
-  axios.get(FETCH_REPOS_URL, {
-    headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
-  }).then((response) => {
-    disptach({ type: FETCH, payload: response.data });
-  }).catch((error) => console.error(error.message));
+export const fetchWatchedRepos = () => (dispatch) => {
+  axios.get(FETCH_REPOS_URL,
+    { headers: { Authorization: `Bearer ${ACCESS_TOKEN}` } })
+    .then((response) => {
+      dispatch({ type: FETCH, payload: response.data });
+    })
+    .catch((error) => console.error(error.message));
 };
 
-export const sortRepos = (sorted) => (disptach) => {
-  disptach({ type: SORT, payload: sorted });
+export const sortRepos = (sorted) => (dispatch) => {
+  dispatch({ type: SORT, payload: sorted });
 };
 
-export const searchRepositories = (query) => (disptach) => {
-  disptach({ type: SEARCH, payload: query });
+export const searchRepositories = (query) => (dispatch) => {
+  dispatch({ type: SEARCH, payload: query });
 };
 
-export const removeRepo = (owner, repoName) => (disptach) => {
+export const removeRepo = (owner, repoName) => (dispatch) => {
   axios.delete(`${REMOVE_REPO_URL}/${owner}/${repoName}/subscription`, {
     headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
-  }).then(
-    () => {
-      disptach({ type: REMOVE, payload: [repoName, owner] });
-    },
-  );
+  })
+    .then(() => {
+      dispatch({ type: REMOVE, payload: [repoName, owner] });
+    }).catch((error) => {
+      console.error(error.message);
+    });
 };
 
 const compare = (a, b) => {
@@ -59,17 +61,21 @@ const reposReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH:
       return { ...state, Repos: action.payload, ReposBackup: action.payload };
+
     case SORT:
     { const sortedRepo = state.Repos.sort((a, b) => compare(a, b));
       return { ...state, Repos: sortedRepo, sorted: action.payload };
     }
+
     case SEARCH: {
       const filtered = filterRepos(action.payload, state.ReposBackup);
       return { ...state, Repos: filtered }; }
+
     case REMOVE: {
       const remained = removeRepoState(action.payload, state.Repos);
       return { ...state, Repos: remained };
     }
+
     default:
       return state;
   }
