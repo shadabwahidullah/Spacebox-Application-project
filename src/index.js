4a62ab6b7c320937ce1e9c5f855b310a0a8e3da5
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 21:18:39 +0430

    Remove test files

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import App from './App';
 import store from './redux/configureStore';
+import App from './App';
+
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
