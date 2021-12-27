88ba717b672ef5a7faa1144e3f80b12efaa55240
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 20:52:48 +0430

    Organize NewIssue

--- src/components/NewIssue.js
+++ src/components/NewIssue.js
@@ -22,10 +22,20 @@ const NewIssue = () => {
 
   return (
     <div className="col mb-4">
-      <form className="p-3 text-center bg-primary rounded py-2 g-2" onSubmit={(event) => { controlForm(event); }}>
+      <form
+        className="p-3 text-center bg-primary rounded py-2 g-2"
+        onSubmit={(event) => { controlForm(event); }}
+      >
         <h3 className="text-light">Create a new Issue</h3>
         <hr className="bg-light" />
-        <input className="form-control" type="text-area" value={data.title} onChange={(e) => handle(e)} required placeholder="Describe the issue here" />
+        <input
+          className="form-control"
+          type="text-area"
+          value={data.title}
+          onChange={(e) => handle(e)}
+          required
+          placeholder="Describe the issue here"
+        />
         <button type="submit" className="btn-info rounded text-light px-4 py-1 mt-3">Create</button>
       </form>
     </div>
