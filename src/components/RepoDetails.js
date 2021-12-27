3ce63235d443f6f1672b11d9981f3ab3b47a7b54
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 15:26:19 +0430

    Add five latest issues

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
 import { getRepo, getIssues } from '../redux/repoDetailsReducer';
+import Issue from './Issue';
 
 const RepoDetails = () => {
   const params = useParams();
@@ -23,43 +24,47 @@ const RepoDetails = () => {
   const { login } = owner;
 
   return (
-    <div key={id} className="p-3 text-center bg-primary rounded py-2 g-2 repo">
-      <h3 className="text-light">{name}</h3>
-      <h6 className="text-light text-start">
-        Description:
-      </h6>
-      <hr className="col-3 bg-light mb-1" />
-      <p className="text-light text-start ps-2">{description}</p>
-      <hr className="bg-light" />
-      <span className="d-flex justify-content-around">
-        <h6 className="text-light">
-          Owner:
-          {' '}
-          {login}
+    <div>
+      <div key={id} className="p-3 text-center bg-primary rounded py-2 g-2 repo">
+        <h3 className="text-light">{name}</h3>
+        <h6 className="text-light text-start">
+          Description:
         </h6>
-        <h6 className="text-light">
-          Open issues:
-          {' '}
-          {openIssues}
-        </h6>
-        <h6 className="text-light">
-          Forks count:
-          {' '}
-          {forksCount}
-        </h6>
-      </span>
-      <span className="text-light pt-4 row">
-        <h6 className="col-md-6">
-          Created At:
-          {' '}
-          {new Date(date).toDateString()}
-        </h6>
-        <h6 className="col-md-6">
-          Number of open PR:
-          {' '}
-          {noPR}
-        </h6>
-      </span>
+        <hr className="col-3 bg-light mb-1" />
+        <p className="text-light text-start ps-2">{description}</p>
+        <hr className="bg-light" />
+        <span className="d-flex justify-content-around">
+          <h6 className="text-light">
+            Owner:
+            {' '}
+            {login}
+          </h6>
+          <h6 className="text-light">
+            Open issues:
+            {' '}
+            {openIssues}
+          </h6>
+          <h6 className="text-light">
+            Forks count:
+            {' '}
+            {forksCount}
+          </h6>
+        </span>
+        <span className="text-light pt-4 row">
+          <h6 className="col-md-6">
+            Created At:
+            {' '}
+            {new Date(date).toDateString()}
+          </h6>
+          <h6 className="col-md-6">
+            Number of open PR:
+            {' '}
+            {noPR}
+          </h6>
+        </span>
+      </div>
+      <h4 className="m-3 mb-1">Latest Open Issues:</h4>
+      {Issues.map((e) => <Issue key={e.id} issue={e} />)}
     </div>
   );
 };
