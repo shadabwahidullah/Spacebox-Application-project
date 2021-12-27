e95b600c91df846d287c62dc2cfcad3b2e8324fc
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 21:34:53 +0430

    Disable loggers for configureStore

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,5 +1,5 @@
 import { applyMiddleware, createStore, combineReducers } from 'redux';
-import logger from 'redux-logger';
+// import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import reposReducer from './reposReducer';
 import newRepoReducer from './newRepoReducer';
@@ -11,6 +11,9 @@ const reducer = combineReducers({
   repoDetailsReducer,
 });
 
-const store = createStore(reducer, applyMiddleware(logger, thunk));
+const store = createStore(reducer, applyMiddleware(thunk));
+// to see state log at each step replace the line above
+// with the line bellow and un-comment the logger import
+// const store = createStore(reducer, applyMiddleware(logger, thunk));
 
 export default store;
