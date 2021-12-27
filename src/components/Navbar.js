2b8b845851f39110a4bef103559b35b7e8ec5cb3
shadabwahidullah <shadabwahidullah@gmail.com>
2021-12-27 21:15:34 +0430

    Remove warnings on navbar active class name

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-light bg-light">
     <div className="container">
-      <Link to="/" exact>
+      <Link to="/" exact="true">
         <h1 className="navbar-brand">Spacebox</h1>
       </Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -13,10 +13,10 @@ const Navbar = () => (
       <div className="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
         <ul className="navbar-nav">
           <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/" exact="true">Home</Link>
+            <Link className="nav-link" to="/" exact="true">Home</Link>
           </li>
           <li className="nav-item">
-            <Link className="nav-link" activeClassName="active" to="/add-new-repo">Add new</Link>
+            <Link className="nav-link" to="/add-new-repo">Add new</Link>
           </li>
         </ul>
       </div>
